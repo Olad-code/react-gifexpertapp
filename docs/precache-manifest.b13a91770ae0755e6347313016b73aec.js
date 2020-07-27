eec3b82786a38df13e13f0f154e2c5d0593af38b
Oscar Sanchez <oscarsanchezfds@gmail.com>
2020-07-27 06:47:09 -0500

    agregado docs

---
+++ docs/precache-manifest.b13a91770ae0755e6347313016b73aec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c3dd86755c7977184f60ad8129a3f3c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "03580cb13ee125d2143f",
+    "url": "/static/css/main.da69ca75.chunk.css"
+  },
+  {
+    "revision": "85a4a86f3fe06f17fcc7",
+    "url": "/static/js/2.8249eaeb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8249eaeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03580cb13ee125d2143f",
+    "url": "/static/js/main.6c955bb0.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
